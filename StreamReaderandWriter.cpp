2dcd4381f7d4741b671fc70050665ddf640bbeb9
muhammadkhalidhidayatullah29-code <muhammadkhalidhidayatullah29@gmail.com>
2026-06-02 17:46:44 +0700

    menambah struktur kendali percabangan if else

--- StreamReaderandWriter.cpp
+++ StreamReaderandWriter.cpp
@@ -45,4 +45,19 @@ int main()
     // kondisi jika file ada
     if (infile.is_open())
     {
+        
+    // perulangan untuk memunculkan setiap baris
+        while (getline(infile, baris))
+        {
+            cout << baris << endl;
+        }
+
+        // menutup file jika telah selesai
+        infile.close();
+    }
+    // kondisi jika file tidak ada
+    else
+        cout << "Unable to open file";
+
+    return 0;
 }
