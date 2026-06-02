d18fb6eb1419fe4b1382b1883ee6f6832794bf4c
muhammadkhalidhidayatullah29-code <muhammadkhalidhidayatullah29@gmail.com>
2026-06-02 18:03:54 +0700

    .

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -49,4 +49,18 @@ int main()
     // kondisi jika file ada
     if (infile.is_open())
     {
+        // perulangan untuk menampilkan setiap baris
+        while (getline(infile, baris))
+        {
+            cout << baris << endl;
+        }
+
+        // menutup file jika telah selesai di loop
+        infile.close();
+    }
+    // kondisi jika file tidak ada
+    else
+        cout << "Unable to open file";
+
+    return 0;
 }
