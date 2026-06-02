17308393ae39fbe95c333ad5bea6d8f6380d416a
muhammadkhalidhidayatullah29-code <muhammadkhalidhidayatullah29@gmail.com>
2026-06-02 17:45:35 +0700

    menambah struktur kendali percabangan if

--- StreamReaderandWriter.cpp
+++ StreamReaderandWriter.cpp
@@ -17,7 +17,32 @@ int main()
     cout << ">= Menulis file, \'q\' untuk keluar" << endl;
 
     // unlimited loop untuk menulis file
-        while (true)
-        {
+    while (true)
+    {
+            cout << "- ";
+            
+                // mendapat setiap karakter dalam satu baris
+                getline(cin, baris);
+            
+                // kondisi false jika baris hanya berisi karakter q
+                if (baris == "q") break;
+        
+                // menulis dan memasukkan nilai dari variabel baris ke dalam file
+                outfile << baris << endl;
+    }
 
+    // menutup file setelah penulisan selesai
+        outfile.close();
+
+    // membuka file dalam mode membaca
+        ifstream infile;
+
+    // menunjuk ke sebuah file
+        infile.open("contohfile.txt");
+
+    cout << endl << ">=Membuka dan membaca file " << endl;
+    
+    // kondisi jika file ada
+    if (infile.is_open())
+    {
 }
