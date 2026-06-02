4c8fb8567063710689a1487e4eb33e2db6eaf5b1
muhammadkhalidhidayatullah29-code <muhammadkhalidhidayatullah29@gmail.com>
2026-06-02 17:58:04 +0700

    menambah catch dengan berbagai kondisi

--- ManualException.cpp
+++ ManualException.cpp
@@ -4,8 +4,23 @@ using namespace std;
 int main()
 {
     // membuat isi dari try untuk pengujian error
-        try
-        {
-            
-        }
+    try
+    {
+        cout << "Selamat belajar di prodi TI UMY" << endl;
+
+        // throw 0.5; // melemparkan sebuah integer maka
+        cout << "Pernyataan tidak akan ditampilkan" << endl;
+    }
+    // kondisi jika throw yang bertipe data integer
+    catch (int a)
+    {
+        cout << "Pengecualian akan di eksekusi" << endl;
+    }
+    // kondisi jika throw bertipe data selain integer
+    catch (...)
+    {
+        cout << "default pengecualian dieksekusi" << endl;
+    }
+
+    return 0;
 }
