400f69ae074aa1ec893efd6451893803ca4e3f65
muhammadkhalidhidayatullah29-code <muhammadkhalidhidayatullah29@gmail.com>
2026-06-02 17:52:32 +0700

    menambah struktur kendali percabangan if else

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -23,5 +23,30 @@ int main()
     // looping untuk menulis isi file
     while (true)
     {
-    
+        cout << "- ";
+        
+            // mendapatkan setiap karakter dalam satu baris
+            getline(cin, baris);
+        
+            // kondisi jika baris hanya berisi huruf q
+            if (baris == "q") break;
+        
+            // menulis dan memasukkan nilai dari baris ke dalam file
+            outfile << baris << endl;
+    }
+
+    // menutup file apabila telah selesai ditulis
+    outfile.close();
+
+    // membuka file dalam mode membaca
+    ifstream infile;
+
+    // menunjuk ke sebuah file dengan ekstensi .txt
+    infile.open(NamaFile + ".txt", ios::in);
+
+    cout << endl << ">= Membuka dan membaca file" << endl;
+
+    // kondisi jika file ada
+    if (infile.is_open())
+    {
 }
